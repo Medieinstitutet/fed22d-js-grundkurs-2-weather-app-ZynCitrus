4fb7e124d08ebabebbc2eead28331c0a95735334
Sebastian Post <sebastianpost@protonmail.com>
2022-12-12 11:20:17 +0100

    Basic JS for reading input & some CSS shenanigans

--- src/main.js
+++ src/main.js
@@ -2,16 +2,24 @@ import './style/style.scss';
 
 const icons = new Image();
 icons.src = "/suncloud.png";
-
-
+let input = '';
+const submitCity = document.querySelector('#submitCity');
 const placeHolderIcon = document.querySelector('#placeHolderIcon');
-let weather = "cloudySun";
+let weather = "sun";
+submitCity.addEventListener('click', returnText);
+
+function returnText() {
+  input = document.getElementById("inputCity").value;
+  localStorage.setItem('input', input);
+  console.log(input);
+}
 
 if (weather === "sun") {
   placeHolderIcon.classList.add("placeHolderIcon");
   placeHolderIcon.classList.remove("cloudySun");
-  console.log(placeHolderIcon);} 
-if (weather === "cloudySun") {
+}if (weather === "cloudySun") {
   placeHolderIcon.classList.add("cloudySun");
   placeHolderIcon.classList.remove("placeHolderIcon");
 };
+
+console.log(input);
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,5 +1,9 @@
 const tempAir = [];
 
 function getTempAir(){
-     
-}
+     fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={75303f12006046a5556974d2d62ef0d1}')
+        .then((data) => data.json())
+        .then((json) => {
+            console.log(json);
+        })
+    }
