de8b323d049f5f4503cd22d2a11adffcac058fad
Sebastian Post <sebastianpost@protonmail.com>
2022-12-14 15:21:42 +0100

    Som JS fixes

--- src/main.js
+++ src/main.js
@@ -1,46 +1,65 @@
 import './style/style.scss';
-import tempArray from './weatherAPI';
+let getCity = 'Stockholm';
+let tempArray = await getTempAir();
 const icons = new Image();
 icons.src = '/suncloud.png';
 const submitCity = document.querySelector('#submitCity');
 const placeHolderIcon = document.querySelector('#placeHolderIcon');
 let clouds = tempArray.clouds.all;
 let weather = '';
-let getCity = 'Stockholm';
 let temp = tempArray.main.temp;
 let city = tempArray.name;
 let rightInDemFeels = tempArray.main.feels_like;
 submitCity.addEventListener('click', returnText);
 document.getElementById("placeHolderTemp").innerHTML = "Idag är det " + temp + ", men det känns som " + rightInDemFeels + " i " + city;
-console.table(tempArray);
 
 /*function to get info from input to getCity */ 
-function returnText() {
+async function returnText() {
   if (document.getElementById('inputCity').value.length == '') {
     alert('Vänligen skriv in något i rutan!')
   } else {
   getCity = document.getElementById('inputCity').value;
   localStorage.setItem('input', getCity);
   console.log(getCity);
+  tempArray = await getTempAir();
+  let clouds = tempArray.clouds.all;
+  let weather = '';
+  let temp = tempArray.main.temp;
+  let city = tempArray.name;
+  let rightInDemFeels = tempArray.main.feels_like;
+  console.log(tempArray)
+  document.getElementById("placeHolderTemp").innerHTML = "Idag är det " + temp + ", men det känns som " + rightInDemFeels + " i " + city;
+  /* if-statement to change theme depending on weather*/  
+  if (weather === 'Sun') {
+    placeHolderIcon.classList.add('placeHolderIcon');
+    placeHolderIcon.classList.remove('cloudySun');
+  }if (weather === 'cloudySun') {
+    placeHolderIcon.classList.add('cloudySun');
+    placeHolderIcon.classList.remove('placeHolderIcon');
+  }if (clouds > 50) {
+    console.log('')
+  }
 }}
 
-/*if-sats for att andra tema beroende pa vader */
 
-if (weather === 'Sun') {
-  placeHolderIcon.classList.add('placeHolderIcon');
-  placeHolderIcon.classList.remove('cloudySun');
-}if (weather === 'cloudySun') {
-  placeHolderIcon.classList.add('cloudySun');
-  placeHolderIcon.classList.remove('placeHolderIcon');
-}if (clouds > 50) {
-  console.log('mordin')
-}
+    async function getTempAir(){
+    let apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${getCity}&appid=75303f12006046a5556974d2d62ef0d1&units=metric`
+     return fetch(apiLink)
+        .then((data) => data.json())
+        .then((json) => {
+            return json;
+        })
+        .catch((error) =>{
+        console.error('Error', error)
+        return null;
+    });
+    };
 
 
-/* En slask för sånt som kan behövas igen sen
+    /* En slask för sånt som kan behövas igen sen
 const tempInAir = document.querySelector('#placeHolderTemp')
 placeHolderCity
 
-
+import tempArray from './weatherAPI';
 
 */
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -3,7 +3,7 @@ const tempArray = await getTempAir();
 
 
     async function getTempAir(){
-    let apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=filipstad&appid=75303f12006046a5556974d2d62ef0d1&units=metric'
+    let apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=75303f12006046a5556974d2d62ef0d1&units=metric'
      return fetch(apiLink)
         .then((data) => data.json())
         .then((json) => {
