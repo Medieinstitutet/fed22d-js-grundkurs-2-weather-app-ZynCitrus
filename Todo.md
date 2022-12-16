bcb3092411c4ffb515b2cc8f66bd20aca9be392d
Sebastian Post <sebastianpost@protonmail.com>
2022-12-16 13:22:01 +0100

    Changing variable name and some CSS apparently.

--- Todo.md
+++ Todo.md
@@ -1,23 +1,14 @@
 ## HTML
-Input field(Stad)
-Submit button
-Placeholder for weather icon(sol, moln, blixt, etc.)
-Placeholder city
-Placeholder tempurature
+
+x Input field(Stad) x Submit button x Placeholder for weather icon(sol, moln, blixt, etc.) x Placeholder city x Placeholder tempurature
 
 ## CSS
-Placeholder for icon - top right
-Placeholder for city - top center
-Placeholder for tempurature - center center
-Style button
-Style input field
+
+Placeholder for icon - top right Placeholder for city - top center Placeholder for tempurature - center center x Style button x Style input field
 
 ## JS
-Const input field
-Get API data(https://openweathermap.org/current)
-Fix Geo Coding(https://openweathermap.org/api/geocoding-api)
-If statement
-- Sky
-- Tempurature
+
+x Const input field x Get API data(https://openweathermap.org/current) Fix Geo Coding(https://openweathermap.org/api/geocoding-api) If statement x - Sky x - Tempurature
+
 - Dark vs light
-- 
+-
--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import './style/style.scss';
-let getCity = 'Stockholm';
-let tempArray = await getTempAir();
+ 
+let getCity = 'oslo';
+let tempuratureArray = await getTempuratureArray();
 const icons = new Image();
 icons.src = '/suncloud.png';
 const submitCity = document.querySelector('#submitCity');
 const placeHolderIcon = document.querySelector('#placeHolderIcon');
-let clouds = tempArray.clouds.all;
-let weather = '';
-let temp = tempArray.main.temp;
-let city = tempArray.name;
-let rightInDemFeels = tempArray.main.feels_like;
+let clouds = tempuratureArray.clouds.all;
+let temp = tempuratureArray.main.temp;
+let tempuratureDecimal = Math.round(temp);
+let city = tempuratureArray.name;
+let rightInDemFeels = tempuratureArray.main.feels_like;
+let feelsLike = Math.round(rightInDemFeels);
 submitCity.addEventListener('click', returnText);
-document.getElementById("placeHolderTemp").innerHTML = "Idag är det " + temp + ", men det känns som " + rightInDemFeels + " i Stockholm";
+document.getElementById("placeHolderTemp").innerHTML = "Idag är det " + tempuratureDecimal + " och det känns som " + feelsLike + " i " + city;
 
 /*function to get info from input to getCity */ 
 async function returnText() {
@@ -20,16 +22,16 @@ async function returnText() {
   } else {
   getCity = document.getElementById('inputCity').value;
   localStorage.setItem('input', getCity);
-  console.log(getCity);
-  tempArray = await getTempAir();
-  let clouds = tempArray.clouds.all;
-  let weather = '';
-  let temp = tempArray.main.temp;
-  let city = tempArray.name;
-  let rightInDemFeels = tempArray.main.feels_like;
-  console.log(tempArray)
-  document.getElementById("placeHolderTemp").innerHTML = "Idag är det " + temp + ", men det känns som " + rightInDemFeels + " i " + city;
-  /* if-statement to change theme depending on weather*/  
+  tempuratureArray = await getTempuratureArray();
+  clouds = tempuratureArray.clouds.all;
+
+  temp = tempuratureArray.main.temp;
+  tempuratureDecimal = Math.round(temp);
+  city = tempuratureArray.name;
+  rightInDemFeels = tempuratureArray.main.feels_like;
+  document.getElementById("placeHolderTemp").innerHTML = "Idag är det " + tempuratureDecimal + " och det känns som " + feelsLike + " i " + city;
+  
+  /* if-statement to change theme depending on weather
   if (weather === 'Sun') {
     placeHolderIcon.classList.add('placeHolderIcon');
     placeHolderIcon.classList.remove('cloudySun');
@@ -37,12 +39,10 @@ async function returnText() {
     placeHolderIcon.classList.add('cloudySun');
     placeHolderIcon.classList.remove('placeHolderIcon');
   }if (clouds > 50) {
-    console.log('')
-  }
+    console.log('fggfhgfh')
+  }*/  
 }}
-
-
-    async function getTempAir(){
+    async function getTempuratureArray(){
     let apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${getCity}&appid=75303f12006046a5556974d2d62ef0d1&units=metric`
      return fetch(apiLink)
         .then((data) => data.json())
@@ -62,6 +62,6 @@ async function returnText() {
 const tempInAir = document.querySelector('#placeHolderTemp')
 placeHolderCity
 
-import tempArray from './weatherAPI';
+import tempuratureArray from './weatherAPI';
 
 */
