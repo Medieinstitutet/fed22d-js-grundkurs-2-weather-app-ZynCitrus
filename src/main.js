cf4c8ca771e5a3d2d21af1fac24a7a9a0188964f
Sebastian Post <sebastianpost@protonmail.com>
2022-12-22 09:58:20 +0100

    Some fixes

--- src/main.js
+++ src/main.js
@@ -12,35 +12,23 @@ let tempuratureDecimal = Math.round(temp);
 let city = tempuratureArray.name;
 let rightInDemFeels = tempuratureArray.main.feels_like;
 let feelsLike = Math.round(rightInDemFeels);
-submitCity.addEventListener('click', returnText);
+submitCity.addEventListener('click', getTextFromInput);
 document.getElementById("placeHolderTemp").innerHTML = "Idag är det " + tempuratureDecimal + " och det känns som " + feelsLike + " i " + city;
 
 /*function to get info from input to getCity */ 
-async function returnText() {
+async function getTextFromInput() {
   if (document.getElementById('inputCity').value.length == '') {
     alert('Vänligen skriv in något i rutan!')
   } else {
   getCity = document.getElementById('inputCity').value;
   localStorage.setItem('input', getCity);
   tempuratureArray = await getTempuratureArray();
-  clouds = tempuratureArray.clouds.all;
-
   temp = tempuratureArray.main.temp;
   tempuratureDecimal = Math.round(temp);
   city = tempuratureArray.name;
   rightInDemFeels = tempuratureArray.main.feels_like;
   document.getElementById("placeHolderTemp").innerHTML = "Idag är det " + tempuratureDecimal + " och det känns som " + feelsLike + " i " + city;
-  
-  /* if-statement to change theme depending on weather
-  if (weather === 'Sun') {
-    placeHolderIcon.classList.add('placeHolderIcon');
-    placeHolderIcon.classList.remove('cloudySun');
-  }if (weather === 'cloudySun') {
-    placeHolderIcon.classList.add('cloudySun');
-    placeHolderIcon.classList.remove('placeHolderIcon');
-  }if (clouds > 50) {
-    console.log('fggfhgfh')
-  }*/  
+    
 }}
     async function getTempuratureArray(){
     let apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${getCity}&appid=75303f12006046a5556974d2d62ef0d1&units=metric`
