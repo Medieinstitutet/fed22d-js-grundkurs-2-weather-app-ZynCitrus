04c069693fde84865d8ba3a1bf2a4f11e61bc6b7
Sebastian Post <sebastianpost@protonmail.com>
2022-12-30 15:33:01 +0100

    Finalization of project. CSS, JS, theme for season

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 import './style/style.scss';
 let checkTime = new Date().getHours();
+let checkMonth = new Date().getMonth();
 const submitCity = document.querySelector('#submitCity');
 submitCity.addEventListener('click', getTextFromInput);
-/*If-sats för att byta bakgrund beroende på tid*/
-if (checkTime >= 7 && checkTime <= 18) {
-  document.body.style.background = "lightblue";
-} else {
-  document.body.style.background = "darkblue";
+
+/*if to change background depending on month*/
+if (checkMonth >=2 && checkMonth <= 4) {
+  console.log('Vår');
+  document.body.style.backgroundImage = "url('/spring.jpg')";
+}if (checkMonth >=5 && checkMonth <= 7) {
+  console.log('Sommar');
+  document.body.style.backgroundImage = "url('/summer.jpg')";
+}if (checkMonth >=8 && checkMonth <= 9) {
+  console.log('Höst');
+  document.body.style.backgroundImage = "url('/fall.jpg')";
+}else{
+  console.log('Vinter');
+  document.body.style.backgroundImage = "url('/winter.jpg')";
 }
+
+
 
 
 
@@ -36,30 +48,24 @@ async function getTempuratureArray(inputCity){
   
 function handleLocation(){
   const successCallback = (position) => {
-    console.log({position});
     let locationLink = `http://api.openweathermap.org/geo/1.0/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&limit=0&appid=75303f12006046a5556974d2d62ef0d1&units=metric`
   return fetch(locationLink)
     .then((data) => data.json())
     .then((json) => {
-      console.log(json);
     getTempuratureArray(json[0].name);
   })
     .catch((error) =>{
-    console.error('Error', error)
     return null;
   });
-
-  };
+};
   
   const errorCallback = (error) => {
-    console.log(error);
   };
   navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
   
 }
 
 function handleData(data){
-  console.log(data)
   const temp = data.main.temp;
   const tempuratureDecimal = Math.round(temp);
   const city = data.name;
@@ -68,7 +74,7 @@ function handleData(data){
   const getIconForSky = data.weather[0].icon;
   const iconForSky = `https://openweathermap.org/img/wn/${getIconForSky}@2x.png`
   document.getElementById('iconForSky').src=iconForSky;
-  document.getElementById("placeHolderTemp").innerHTML = "Idag är det " + tempuratureDecimal + " och det känns som " + feelsLike + " i " + city;
+  document.getElementById("placeHolderTemp").innerHTML = "Idag är det " + tempuratureDecimal + " grader och det känns som " + feelsLike + " i " + city + " grader.";
   // localStorage.setItem('input', getCity);
 };
 
@@ -90,5 +96,12 @@ const tempInAir = document.querySelector('#placeHolderTemp')
 placeHolderCity
 
 import tempuratureArray from './weatherAPI';
+
+
+if (checkTime >= 7 && checkTime <= 18) {
+  document.body.style.background = "lightblue";
+} else {
+  document.body.style.background = "darkblue";
+}
 
 */
